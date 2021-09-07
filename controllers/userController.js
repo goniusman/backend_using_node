1e259ad09718d10089cb87c3fd3ab3750e93d8ce
goniusman <goniusman400@gmail.com>
2021-09-07 08:40:10 +0600

    add and commit message

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const cloudinary = require('../helper/imageUpload');
-const User = require("../models/user");
+const User = require("../models/User");
 const registerValidator = require("../validator/registerValidator");
 const loginValidator = require("../validator/loginValidator");
 
